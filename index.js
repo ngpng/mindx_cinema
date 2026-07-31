d5bbdc971c5567ed6a2e50e6a063c653eb0f7e9a
ngpng <ngongtran0@gmail.com>
2026-07-31 20:46:05 +0700

    change username

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function login(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if(username === "minh" && password === "123"){
+    if(username === "ngpng" && password === "123"){
         alert("success");
     } 
     else {
